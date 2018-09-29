37d7df7ce4d025806a7194fbbabcf8e483d4fd4c
jlog180924 <jlog180924@info.local>
2018-09-28 22:07:12 -0300

    aula 005

---
+++ aulas/workspace/instrucoes-java/src/exercicios/Ex003diaDaSemana.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+
+public class Ex003diaDaSemana {
+public static void main(String[] args) {
+	int numeroPar = 4;
+
+	if (numeroPar % 2 == 0) {
+		System.out.println("É Par");
+
+			
+	} else { 
+
+		System.out.println("é inpar");
+
+	}
+
+}
+
+}
+
+
+
+		
+
+	
+
