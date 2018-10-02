926e52e8e508d81242ded45a122936da0aed398c
jlog180924 <jlog180924@info.local>
2018-10-01 22:00:48 -0300

    aula 006

---
+++ aulas/workspace/array-java/src/exemplos/Array.java
@@ -0,0 +1,29 @@
+package exemplos;
+
+import java.util.Arrays;
+
+public class Array {
+	public static void main(String[] args) {
+		double precos[] = new double [5];
+		
+		String euMeAm[] = {"no", "seu", "coração"};
+				
+		double[] salarios = new double [6];
+		
+		int[] numeros = { 1,2, 3, 4};
+		
+		System.out.println(Arrays.toString(euMeAm));
+		
+		System.out.println(numeros[2]);
+		
+		
+		System.out.println(Arrays.toString(salarios));
+		
+		salarios [2] = 1200.40;
+		System.out.println(Arrays.toString(salarios));
+		
+	}
+
+		
+}
+
---
+++ aulas/workspace/loops-java/src/exemplos/DoWhile.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class DoWhile {
+	public static void main(String[] args) {
+		int num = 1;
+		
+		do {
+			System.out.println("olha eu aqui pela " + num + " ª vez!");
+			++num;
+			
+			
+		} while (num <10);
+		
+	}
+}
